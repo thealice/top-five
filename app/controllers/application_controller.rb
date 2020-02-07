b72f98ebbbb198399ec8299953da4a18b74eaee4
Alice Freda <alicefreda@gmail.com>
2020-02-06 21:33:34 -0800

    new list form and list items creation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,14 +27,25 @@ class ApplicationController < Sinatra::Base
       @current_user ||= User.find_by(:username => session[:username]) if session[:username]
     end
 
+    def owner
+      # binding.pry
+      @owner ||= User.find_by(:id => params[:id]) if params[:id]
+    end
+
+    def is_owner?
+      !!(owner == current_user)
+    end
+
     def login(username, password)
       user = User.find_by(:username => username) 
  
       if user && user.authenticate(password)
         session[:username] = user.username
+        session[:user_id] = user.id
       else
         redirect to '/login'
       end
+      
     end
 
     def logout!
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,17 +2,42 @@ class ListsController < ApplicationController
 
   # GET: /lists
   get "/lists" do
+    @lists = List.all
     erb :"/lists/index.html"
   end
 
   # GET: /lists/new
   get "/lists/new" do
-    erb :"/lists/new.html"
+    if logged_in?
+      erb :"/lists/new.html"
+    else
+      redirect to "/login"
+    end
   end
 
   # POST: /lists
   post "/lists" do
-    redirect "/lists"
+    if logged_in?
+
+      @list = List.new(title: params[:title])
+      @user = current_user
+      if @list && @list.save
+        @list.category = params[:category] if params[:category] != ""
+        @list.user_id = current_user.id
+        params[:list_items].each_with_index do |item, index|
+          if item["content"] != ""
+            @list.list_items.create(:content => params["list_items"][index]["content"], :rank => index)
+          end
+        end
+        @list.save
+        @user.lists << @list
+        redirect to "/users/#{current_user.id}"
+      else
+        erb :"/lists/new.html"
+      end
+    else
+      erb :error
+    end
   end
 
   # GET: /lists/5
@@ -20,9 +45,14 @@ class ListsController < ApplicationController
     erb :"/lists/show.html"
   end
 
-  # GET: /lists/5/edit
+  # GET: /lists/5/edit - make sure to limit to owner of lists
   get "/lists/:id/edit" do
-    erb :"/lists/edit.html"
+    if is_owner?
+      @list = List.find_by(id: params[:id])
+      erb :"/lists/edit.html"
+    else
+      erb :error
+    end
   end
 
   # PATCH: /lists/5
