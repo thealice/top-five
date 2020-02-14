cc456fedc649a7c3919825365cec4ca4c3e587da
Alice Freda <alicefreda@gmail.com>
2020-02-14 15:15:30 -0800

    create db table for categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_06_001831) do
+ActiveRecord::Schema.define(version: 2020_02_14_231028) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.integer "list_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "list_items", force: :cascade do |t|
     t.integer "rank"
